8ff720376b9d92f2fcf441d3bc649fec6e91e648
amir <ahamadzadeh1378@gmail.com>
2019-11-30 21:51:27 +0330

    multi langs confug in app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Routes from './routes/Routes';
 import Academy from './containers/Academy/Academy' ; 
 
 
+
+import counterpart from 'counterpart' ; 
+import fa from './langs/fa' ; 
+import en from './langs/en' ; 
+counterpart.registerTranslations('fa' ,fa) ;
+
+counterpart.setLocale('fa') ; 
+
+
+
+
 export default function (){
   return (
     <React.Fragment>
