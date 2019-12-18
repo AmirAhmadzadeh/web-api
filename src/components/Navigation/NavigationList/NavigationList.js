eca797331b531ef67657d005a313ea23a5676d22
amir <ahamadzadeh1378@gmail.com>
2019-12-18 20:34:30 +0330

    adding one static Navigation Item

--- src/components/Navigation/NavigationList/NavigationList.js
+++ src/components/Navigation/NavigationList/NavigationList.js
@@ -5,16 +5,27 @@ import NavigationItem from './../NavigationItem/NavigationItem';
 const navigationList = (props) => {
     return (
         <ul className="navigation__list">
+
+            
+            <NavigationItem
+                    link='/academy'
+                    name='آکادمی'
+                    class="navigation__item"
+                    linkClass="navigation__link"
+                />)
+            
+            
             {
+
                 props.menuItems ? props.menuItems.map(item => {
                     return (
                         <NavigationItem
                             link={item.link}
                             name={item.name}
-                            class="navigation__item" 
+                            class="navigation__item"
                             linkClass="navigation__link"
-                        />)
-                }) : null 
+                        /> ) 
+                }) : null
             }
         </ul>
     );
