ab4fd82071b5b0e8fade8b054f0c180c14f4bfdb
amir <ahamadzadeh1378@gmail.com>
2019-12-18 20:34:51 +0330

    removing unneccery imports

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,9 +2,9 @@
 
 
 
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react' ; 
 import Header from '../../components/Header/Header';
-import { connect, useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getHome, getUserData, logout, getAcademy, registerCourse, getSinglePageCourseInfo, getCanUseCourseForUser } from '../../store/actions';
 import { withRouter } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
@@ -30,11 +30,14 @@ function home(props) {
   const canUseCourse = useSelector(state => state.academy.canUseCourse)
   const menus = useSelector(state => state.home.menus);
   const auth = useSelector(state => state.auth);
-  const courses = useSelector(state => state.academy.courses)
+  const courses = useSelector(state => state.academy.courses)   ;  
   const dispatch = useDispatch();
+
   const loadHome = () => dispatch(getHome());
   const loadUserData = () => dispatch(getUserData());
+
   const userLogout = () => dispatch(logout());
+
   const loadCourses = () => dispatch(getAcademy());
 
   const showAuthPage = () => {
@@ -52,12 +55,13 @@ function home(props) {
 
   }
   const registerCHandler = () => dispatch(registerCourse());
-  const selectedCourse = useSelector(state => state.academy.selectedCourse)
+  const selectedCourse = useSelector(state => state.academy.selectedCourse)     ;  
   function registerCourseHandler() {
     registerCHandler(selectedCourse);
   }
   const loadSingleCoursePage = (id) => dispatch(getSinglePageCourseInfo(id));
   const getCanUse = (id) => dispatch(getCanUseCourseForUser(id));
+  
   function handleClickItem(courseId) {
     loadSingleCoursePage(courseId);
     getCanUse(courseId);
